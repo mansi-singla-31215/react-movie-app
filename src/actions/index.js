81160639ed31b2594b72581385b0bd8f859bc194
mansisingla31215@gmail.com <83231180+mansi-singla-31215@users.noreply.github.com>
2022-01-22 23:13:34 +0530

    changing state to add favourites as well

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,16 @@
+import movies from "../reducers";
+
 {
     // type: 'ADD_MOVIES'
+}
+
+//action types 
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+// action creators
+export function addMovies(movies){
+    return{
+        type: ADD_MOVIES,
+        movies
+    }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import React from 'react';
+import {addMovies} from '../actions';
 
 class App extends React.Component {
   componentDidMount(){
@@ -12,16 +13,13 @@ class App extends React.Component {
     });
 
     //dispatch action
-      store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data
-    });
+      store.dispatch(addMovies(data));
 
     console.log('STATE', this.props.store.getState());
   }
 
   render(){
-    const movies = this.props.store.getState();
+    const {list} = this.props.store.getState();
   return (
     <div className="App">
       <Navbar />
@@ -32,7 +30,7 @@ class App extends React.Component {
         </div>
         
         <div className="list">
-          {movies.map((movie, index) => (
+          {list.map((movie, index) => (
             <MovieCard movie={movie} key={`movies-${index}`} />
           ))}
         </div>
