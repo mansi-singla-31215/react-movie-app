6a5f60e59aed0383457d916f95265c86a4c00eaf
mansisingla31215@gmail.com <83231180+mansi-singla-31215@users.noreply.github.com>
2022-01-23 20:56:42 +0530

    Added search result

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, CreateStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+// function logger(obj, next, action) -> currying func
+// const logger = function ({dispatch, getState}) {
+//   return function(next){
+//     return function(action){
+//       // middleware code
+//       console.log('ACTION_TYPE = ', action.type);
+//       next(action);
+//     }
+//   }
+// }
+
+const logger = ({dispatch, getState}) => (next) => (action) => {
+  //logger code
+  if(typeof action !== 'function') {
+    console.log('ACTION_TYPE = ', action.type);
+  }
+  next(action);
+}
+
+// const thunk = ({dispatch, getState}) => (next) => (action) => {
+//   if(typeof action === 'function') {
+//     action(dispatch);
+//     return;
+//   }
+//   next(action);
+// }
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 console.log('state', store.getState());
 
 // store.dispatch({
